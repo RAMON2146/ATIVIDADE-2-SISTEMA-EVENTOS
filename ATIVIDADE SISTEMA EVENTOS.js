68a40c478b6457fbde39cdcb74f2e6d95767ef4d
rawsantos01 <ramonssmdanilorr.24@gmail.com>
2021-12-22 17:12:42 -0300

    main

---
+++ ATIVIDADE SISTEMA EVENTOS.js
@@ -0,0 +1,53 @@
+let palestrantes = 10
+
+if(palestrantes <= 10) {
+    console.log("Número de Palestrantes é satisfatório, vamos prosseguir com o cadastro!!")
+} else {
+    console.log("Palestrantes foi excedido, evento não será cadastrado.")
+}
+
+let participantes = 100
+
+if(participantes <= 100) {
+    console.log ("Número de Participantes é satisfatório, vamos prosseguir com o cadastro!!")
+} else {
+    console.log("Participantes foi excedido, evento não será cadastrado.")
+}
+
+let idade = '18'
+
+console.log("Sua idade é:", idade.length)
+
+if(idade.length >= 18) {
+    console.log("Cadastro permitido, registre o evento")
+} else {
+    console.log("Cadastro não permitido")
+}
+
+let idadepalestrante = '18'
+
+console.log("Idade dos palestrantes é:", idadepalestrante.length)
+
+if(idade.length >= 18) {
+    console.log("Cadastro permitido ao palestrante, registre o evento")
+} else {
+    console.log("Cadastro não permitido")
+}
+
+let data = '20'
+
+console.log("A data para o evento é até o dia:", data.length)
+
+if(data.length > 20) {
+    console.log("Evento validado, continue o cadastro")
+} else {
+    console.log("Data inválida, cadastro não permitido")
+}
+
+let listarparticipantes = ['']
+
+console.log(listarparticipantes.length)
+
+let listarpalestrantes = ['']
+
+console.log(listarpalestrantes.length)
